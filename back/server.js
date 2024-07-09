e8b98dd700907f38bec94ac078583cbc107df69c
kdelosrios <kvivi1407@gmail.com>
2024-07-08 19:15:35 -0500

    Se crear el server

---
+++ back/server.js
@@ -0,0 +1,12 @@
+const app=require("./app")
+
+// setear el archivo de configuración
+
+const dotenv=require("dotenv");
+dotenv.config({path: 'back/config/.env'})
+
+const server=app.listen(process.env.PORT, ()=>{
+    console.log(`Servidor iniciado en el puerto ${process.env.PORT} en modo ${process.env.NODE_ENV}`)
+})
+
+
