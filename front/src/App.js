250cd01b64d222c41718c35bc7cae18785829761
kdelosrios <kvivi1407@gmail.com>
2024-07-28 15:09:42 -0500

    Se crea el fron con react

---
+++ front/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+          
+      <img src='./system.png'></img>
+      
+    </div>
+  );
+}
+
+export default App;
